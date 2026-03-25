cdf0f71c111a148f8159d7f95dc203477cc99891
kurtmayan <johnkurt@mayan.com.ph>
2026-03-25 18:03:30 +0800

    feat: added remember me UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
-import { Field, FieldLabel } from "@/components/ui/field"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 
@@ -32,6 +33,25 @@ export default function LoginPage() {
             className="h-11"
           />
         </Field>
+        <div className="flex items-center justify-between">
+          <FieldGroup className="w-56">
+            <Field orientation="horizontal">
+              <Checkbox id="terms-checkbox-basic" name="terms-checkbox-basic" />
+              <FieldLabel
+                htmlFor="terms-checkbox-basic"
+                className="text-[13px] font-medium text-navy-blue"
+              >
+                Remember me
+              </FieldLabel>
+            </Field>
+          </FieldGroup>
+          <Button
+            variant={"link"}
+            className="text-[13px] font-medium text-navy-blue underline"
+          >
+            Forgot password?
+          </Button>
+        </div>
         <Button size={"lg"} className="text-[16px] font-semibold">
           Sign In
         </Button>
@@ -39,7 +59,7 @@ export default function LoginPage() {
       <div>
         <Separator className="mb-5" />
         <p className="text-center text-sm text-[#8A96A3]">
-          Powered by Mayan Solutions Inc.
+          System Version 1.0.0 • Mayan Solutions Inc.
         </p>
       </div>
     </form>
